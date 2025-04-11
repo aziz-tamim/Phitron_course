aa323533d38109221da5518eec07a361f6d7a8cc
abdul-aziz <tamimaziz2007@gmail.com>
2025-04-11 09:30:33 +0600

    update

--- Introduction to Programming/Module 1/input.c
+++ Introduction to Programming/Module 1/input.c
@@ -1,10 +1,22 @@
+// #include<stdio.h>
+// int main()
+// {
+//     int a;
+//     float f;
+//     char c;
+//     scanf("%d %f %c", &a, &f, &c);
+//     printf("%d %f %c", a,f,c);
+//     return 0;
+// }
+
 #include<stdio.h>
 int main()
 {
     int a;
-    float f;
-    char c;
-    scanf("%d %f %c", &a, &f, &c);
-    printf("%d %f %c", a,f,c);
+    long long int b;
+    double c;
+    char d;
+    scanf("%d %lld %lf %c", &a, &b, &c, &d);
+    printf("%d\n%lld\n%lf\n%c\n",a,b,c,d);
     return 0;
 }
