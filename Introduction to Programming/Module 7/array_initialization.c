151275df153c8d5e2bb779676744f2dc8f6121a7
abdul-aziz <tamimaziz2007@gmail.com>
2025-04-17 08:09:44 +0600

    update

---
+++ Introduction to Programming/Module 7/array_initialization.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int x = 10; // declare and initialize
+    int a[5] = {10,20,30,40,50};  // declare and initialize
+    for(int i=0;i<5;i++)
+    {
+        printf("%d ",a[i]);
+    }
+    return 0;
+}
---
+++ Introduction to Programming/Module 7/array_input_and_output.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int a[5];
+    // scanf("%d %d %d %d %d",&a[0], &a[1], &a[2], &a[3], &a[4]);
+    // printf("%d %d %d %d %d",a[0], a[1], a[2], a[3], a[4]);
+    for(int i=0;i<=4;i++)
+    {
+        scanf("%d\n",&a[i]);
+    }
+    for(int i=0;i<=4;i++)
+    {
+        printf("%d\n",a[i]);
+    }
+    return 0;
+}
