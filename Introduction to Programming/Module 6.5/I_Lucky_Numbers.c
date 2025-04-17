bf2f930bee8f00824444694a3233373d49c801ec
abdul-aziz <tamimaziz2007@gmail.com>
2025-04-17 07:07:33 +0600

    update

--- Introduction to Programming/Module 6.5/I_Lucky_Numbers.c
+++ Introduction to Programming/Module 6.5/I_Lucky_Numbers.c
@@ -15,25 +15,26 @@
 // }
 
 
-#include<stdio.h>
-int main()
-{
-    int n;
-    scanf("%n",&n);
+// #include<stdio.h>
+// int main()
+// {
+//     int n;
+//     scanf("%n",&n);
 
-    int first_digit = n/10;
-    int second_digit = n%10;
-    if(second_digit==0)
-    {
-        printf("YES\n");
-    }
-    else if(first_digit%second_digit==0 || second_digit%first_digit==0)
-    {
-        printf("YES\n");
-    }
-    else
-    {
-        printf("NO\n");
-    }
-    return 0;
-}
+//     int first_digit = n/10;
+//     int second_digit = n%10;
+//     if(second_digit==0)
+//     {
+//         printf("YES\n");
+//     }
+//     else if(first_digit%second_digit==0 || second_digit%first_digit==0)
+//     {
+//         printf("YES\n");
+//     }
+//     else
+//     {
+//         printf("NO\n");
+//     }
+//     return 0;
+// }
+
