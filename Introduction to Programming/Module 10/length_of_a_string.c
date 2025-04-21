ab55c25c19f8d8899b9ab48cd837b7c7fe1f01a0
abdul-aziz <tamimaziz2007@gmail.com>
2025-04-21 14:11:34 +0600

    update

---
+++ Introduction to Programming/Module 10/length_of_a_string.c
@@ -0,0 +1,26 @@
+// #include<stdio.h>
+// int main()
+// {
+//     char s[101];
+//     scanf("%s",s);
+//     int count = 0;
+//     for (int i=0;s[i] !='\0';i++)
+//     {
+//         count++;
+//     }
+//     printf("%d",count);
+//     return 0;
+// }
+
+
+// Length of a string using strlen
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char s[101];
+    scanf("%s",s);
+    int size = strlen(s);
+    printf("%d",size);
+    return 0;
+}
---
+++ Introduction to Programming/Module 10/string_initialization.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main()
+{
+    // char s[6] = {'T','A','M','I','M'};
+    char s[6] = "TAMIM";
+    printf("%s",s);
+    return 0;
+}
