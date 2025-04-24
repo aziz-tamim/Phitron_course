63ad448e748dcc7807ef04ed1aa11b08d47822c0
abdul-aziz <tamimaziz2007@gmail.com>
2025-04-24 08:04:23 +0600

    update

---
+++ Introduction to Programming/Module 11.5 Practice Day/frequency_array.c
@@ -0,0 +1,118 @@
+// #include<stdio.h>
+// int main()
+// {
+//     int n;
+//     scanf("%d",&n);
+//     int a[n];
+//     for(int i=0;i<n;i++)
+//     {
+//         scanf("%d",&a[i]);
+//     }
+//     int cnt0 = 0, cnt1 = 0, cnt2 = 0, cnt3 = 0, cnt4 = 0,cnt5 = 0;
+//     for(int i=0;i<n;i++)
+//     {
+//         if(a[i] == 0)
+//         {
+//             cnt0++;
+//         }
+//         else if(a[i] == 1)
+//         {
+//             cnt1++;
+//         }
+//         else if(a[i] == 2)
+//         {
+//             cnt2++;
+//         }
+//         else if(a[i] == 3)
+//         {
+//             cnt3++;
+//         }
+//         else if(a[i] == 4)
+//         {
+//             cnt4++;
+//         }
+//         else if(a[i] == 5)
+//         {
+//             cnt5++;
+//         }
+//     }
+//     printf("%d --> %d\n",0, cnt0);
+//     printf("%d --> %d\n",1, cnt1);
+//     printf("%d --> %d\n",2, cnt2);
+//     printf("%d --> %d\n",3, cnt3);
+//     printf("%d --> %d\n",4, cnt4);
+//     printf("%d --> %d\n",5, cnt5);
+//     return 0;
+// }
+
+// #include<stdio.h>
+// int main()
+// {
+//     int n;
+//     scanf("%d",&n);
+//     int a[n];
+//     for(int i=0;i<n;i++)
+//     {
+//         scanf("%d",&a[i]);
+//     }
+//     int frequency[6] = {0};
+//     for(int i=0;i<n;i++)
+//     {
+
+//         if(a[i] == 0)
+//         {
+//             frequency[0]++;
+//         }
+//         else if(a[i] == 1)
+//         {
+//             frequency[1]++;
+//         }
+//         else if(a[i] == 2)
+//         {
+//             frequency[2]++;
+//         }
+//         else if(a[i] == 3)
+//         {
+//             frequency[3]++;
+//         }
+//         else if(a[i] == 4)
+//         {
+//             frequency[4]++;
+//         }
+//         else if(a[i] == 5)
+//         {
+//             frequency[5]++;
+//         }
+//     }
+//     printf("%d --> %d\n",0, frequency[0]);
+//     printf("%d --> %d\n",1, frequency[1]);
+//     printf("%d --> %d\n",2, frequency[2]);
+//     printf("%d --> %d\n",3, frequency[3]);
+//     printf("%d --> %d\n",4, frequency[4]);
+//     printf("%d --> %d\n",5, frequency[5]);
+//     return 0;
+// }
+
+
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int a[n];
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    int fre[6] = {0};
+    for(int i=0;i<n;i++)
+    {
+        int val = a[i];
+        fre[val]++;
+    }
+    for(int i=0;i<6;i++)
+    {
+        printf("%d --> %d\n",i, fre[i]);
+    }
+    return 0;
+}
