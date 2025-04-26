61414992c9cd857fa10ac0d408f041f958c41699
abdul-aziz <tamimaziz2007@gmail.com>
2025-04-26 08:35:59 +0600

    update

--- Introduction to Programming/Module 10/string_intput_with_space.c
+++ Introduction to Programming/Module 10/string_intput_with_space.c
@@ -1,11 +1,11 @@
-// #include<stdio.h>
-// int main()
-// {
-//     char s[50];
-//     gets(s);
-//     printf("%s",s);
-//     return 0;
-// }
+#include<stdio.h>
+int main()
+{
+    char s[50];
+    gets(s);
+    printf("%s",s);
+    return 0;
+}
 
 #include<stdio.h>
 int main()
