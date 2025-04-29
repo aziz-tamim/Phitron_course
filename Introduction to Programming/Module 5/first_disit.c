db113596a4c44d473a288f40f8b58c8daf97123d
abdul-aziz <tamimaziz2007@gmail.com>
2025-04-29 08:05:57 +0600

    update

--- Introduction to Programming/Module 5/first_disit.c
+++ Introduction to Programming/Module 5/first_disit.c
@@ -1,20 +1,20 @@
 // 4 ta shonkar modde shudu first disgit ta nite se.
-// #include<stdio.h>
-// int main()
-// {
-//     int n;
-//     scanf("%d",&n);
-//     int first_digit = n/1000;
-//     if(first_digit%2==0)
-//     {
-//         printf("EVEN");
-//     }
-//     else
-//     {
-//         printf("ODD");
-//     }
-//     return 0;
-// }
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int first_digit = n/1000;
+    if(first_digit%2==0)
+    {
+        printf("EVEN");
+    }
+    else
+    {
+        printf("ODD");
+    }
+    return 0;
+}
 
 
 // 4 ta shonkar modde shudu last disgit ta nite se.
