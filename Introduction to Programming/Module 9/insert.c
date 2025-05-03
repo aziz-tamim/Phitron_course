333f52a5af83408dc434a4daa61e4dc3087b24fc
abdul-aziz <tamimaziz2007@gmail.com>
2025-05-03 10:52:37 +0600

    update

--- Introduction to Programming/Module 9/insert.c
+++ Introduction to Programming/Module 9/insert.c
@@ -4,17 +4,18 @@ int main()
     int n;
     scanf("%d",&n);
     int a[n+1];
-    for (int i=0;i<n;i++)
+    for(int i=0;i<n;i++)
     {
         scanf("%d",&a[i]);
     }
-    int idx, val;
-    scanf("%d %d", &idx, &val);
-    for(int i=n;i >= idx+1; i--)
+    int idx,val;
+    scanf("%d %d",&idx,&val);
+    for(int i=n;i>=idx+1; i--)
     {
         a[i] = a[i-1];
     }
     a[idx] = val;
+
     for(int i=0;i<=n;i++)
     {
         printf("%d ",a[i]);
