ed79aa486d209569bbfc5758922e344753c23347
abdul-aziz <tamimaziz2007@gmail.com>
2025-05-08 07:37:54 +0600

    update

--- Introduction to Programming/Module 15/function_with_array.c
+++ Introduction to Programming/Module 15/function_with_array.c
@@ -1,37 +1,37 @@
-#include<stdio.h>
-void fun(int a[])
-{
-    a[1] = 200;
-}
-int main()
-{
-    int a[5] = {10,20,30,40,50};
-    fun(a);
-    for(int i=0; i<5; i++)
-    {
-        printf("%d ",a[i]);
-    }
-    return 0;
-}
-
-
 // #include<stdio.h>
-// void fun(int a[], int n)
+// void fun(int a[])
 // {
-//     for(int i=0; i<n; i++)
-//     {
-//         printf("%d ",a[i]);
-//     }
+//     a[1] = 200;
 // }
 // int main()
 // {
-//     int n;
-//     scanf("%d",&n);
-//     int a[n];
-//     for(int i=0; i<n; i++)
+//     int a[5] = {10,20,30,40,50};
+//     fun(a);
+//     for(int i=0; i<5; i++)
 //     {
-//         scanf("%d ",&a[i]);
+//         printf("%d ",a[i]);
 //     }
-//     fun(a,n);
 //     return 0;
 // }
+
+
+#include<stdio.h>
+void fun(int a[], int n)
+{
+    for(int i=0; i<n; i++)
+    {
+        printf("%d ",a[i]);
+    }
+}
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int a[n];
+    for(int i=0; i<n; i++)
+    {
+        scanf("%d ",&a[i]);
+    }
+    fun(a,n);
+    return 0;
+}
