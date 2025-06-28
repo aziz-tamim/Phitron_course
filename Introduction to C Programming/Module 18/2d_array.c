c4904e36291e176439f7d5092a5a078b8c718ebf
abdul-aziz <tamimaziz2007@gmail.com>
2025-06-28 09:26:09 +0600

    update

--- Introduction to C Programming/Module 18/2d_array.c
+++ Introduction to C Programming/Module 18/2d_array.c
@@ -12,14 +12,14 @@
 int main()
 {
     int r,c;
-    scanf("%d %d",&r, &c);
+    scanf("%d %d", &r, &c);
     int a[r][c];
 
     for(int i=0; i<r; i++)
     {
         for(int j=0; j<c; j++)
         {
-            scanf("%d",&a[i][j]);
+            scanf("%d", &a[i][j]);
         }
     }
 
