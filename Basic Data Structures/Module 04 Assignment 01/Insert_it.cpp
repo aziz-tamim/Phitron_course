9e26e43c31900a578e5885509b55e6eda384a73d
abdul-aziz <tamimaziz2007@gmail.com>
2025-06-28 08:55:29 +0600

    update'

--- Basic Data Structures/Module 04 Assignment 01/Insert_it.cpp
+++ Basic Data Structures/Module 04 Assignment 01/Insert_it.cpp
@@ -7,7 +7,7 @@ int main()
     vector<int> a(n);
     for(int i=0; i<n; i++)
     {
-        cin >> a[i];
+        cin >> a[i]; 
     }
 
     int m;
--- Basic Data Structures/Module 04 Assignment 01/Printing_X.cpp
+++ Basic Data Structures/Module 04 Assignment 01/Printing_X.cpp
@@ -6,7 +6,33 @@ int main()
     cin >> n;
     for(int i=0; i<n; i++)
     {
-
+        for(int l =0; l <n; l++)
+        {
+            if(i== n/2 && l==n/ 2)
+            {
+                cout << "X";
+            }
+            else if(i< n/2 && l == i)
+            {
+                cout << "\\";
+            }
+            else if(i< n/2 && l == n-i-1)
+            {
+                cout <<  "/" ;
+            }
+            else if(i>n/2 && l== n-i-1)
+            {
+                cout << "/";
+            }
+            else if(i> n/2 && l == i)
+            {
+                cout << "\\";
+            }
+            else{
+                cout << " ";
+            }
+        }
+            cout << endl;
     }
     return 0;
 }
