a72470f312558ad97779b44864f69031da175ceb
abdul-aziz <tamimaziz2007@gmail.com>
2025-06-29 15:32:40 +0600

    update

---
+++ Basic Data Structures/Module 05/linked_list.cpp
@@ -0,0 +1,17 @@
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+//     int a[5] = {1,2,3,4,5};
+//     cout << (int)&a[2] << " " << (int)&a[3];
+//     return 0;
+// }
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    vector<int> v = {1,2,3,4,5};
+    cout << (int)&v[2] << " " << (int)&v[3];
+    return 0;
+}
