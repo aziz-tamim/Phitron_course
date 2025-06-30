6a3c63ce45d55e7024b707e1a60bdca5568303b8
abdul-aziz <tamimaziz2007@gmail.com>
2025-06-30 15:29:21 +0600

    update

--- Basic Data Structures/Module 05/linked_list.cpp
+++ Basic Data Structures/Module 05/linked_list.cpp
@@ -7,11 +7,21 @@
 //     return 0;
 // }
 
-#include<bits/stdc++.h>
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+//     vector<int> v = {1,2,3,4,5};
+//     cout << (int)&v[2] << " " << (int)&v[3];
+//     return 0;
+// }
+
+#include <bits/stdc++.h>
 using namespace std;
+
 int main()
 {
-    vector<int> v = {1,2,3,4,5};
-    cout << (int)&v[2] << " " << (int)&v[3];
+    int a[5] = {1, 2, 3, 4, 5};
+    cout << (uintptr_t)&a[2] << " " << (uintptr_t)&a[3];
     return 0;
 }
--- Basic Data Structures/Module 05/printing_linked_list.cpp
+++ Basic Data Structures/Module 05/printing_linked_list.cpp
@@ -18,7 +18,7 @@ int main()
     Node* head = new Node(10);
     Node* a = new Node(20);
     Node* b = new Node(30);
-    Node* c = new Node(400); 
+    Node* c = new Node(40);
 
     head->next = a;
     a->next = b;
