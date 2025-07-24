4d139aa307c1ff6e5eecc20dd829e9994ccb21ca
abdul-aziz <tamimaziz2007@gmail.com>
2025-07-24 13:36:09 +0600

    update

--- Basic Data Structures/Module 04 Assignment 01/Printing_X.cpp
+++ Basic Data Structures/Module 04 Assignment 01/Printing_X.cpp
@@ -1,38 +1,72 @@
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+//     int n;
+//     cin >> n;
+//     for(int i=0; i<n; i++)
+//     {
+//         for(int l =0; l <n; l++)
+//         {
+//             if(i== n/2 && l==n/ 2)
+//             {
+//                 cout << "X";
+//             }
+//             else if(i< n/2 && l == i)
+//             {
+//                 cout << "\\";
+//             }
+//             else if(i< n/2 && l == n-i-1)
+//             {
+//                 cout <<  "/";
+//             }
+//             else if(i>n/2 && l== n-i-1)
+//             {
+//                 cout << "/";
+//             }
+//             else if(i> n/2 && l == i)
+//             {
+//                 cout << "\\";
+//             }
+//             else{
+//                 cout << " ";
+//             }
+//         }
+//             cout << endl;
+//     }
+//     return 0;
+// }
+
+
 #include<bits/stdc++.h>
 using namespace std;
 int main()
 {
     int n;
     cin >> n;
+    int mid = n/2;
     for(int i=0; i<n; i++)
     {
-        for(int l =0; l <n; l++)
+        for(int j=0; j<n; j++)
         {
-            if(i== n/2 && l==n/ 2)
+            if(i==mid && j == mid)
             {
                 cout << "X";
             }
-            else if(i< n/2 && l == i)
+            else if( i==j)
             {
                 cout << "\\";
             }
-            else if(i< n/2 && l == n-i-1)
-            {
-                cout <<  "/" ;
-            }
-            else if(i>n/2 && l== n-i-1)
+            else if(i+j == n-1)
             {
                 cout << "/";
             }
-            else if(i> n/2 && l == i)
+            else 
             {
-                cout << "\\";
-            }
-            else{
                 cout << " ";
             }
         }
-            cout << endl;
+        cout << endl;
     }
     return 0;
 }
---
+++ Basic Data Structures/Module 15.5/problem_link.cpp
@@ -0,0 +1,16 @@
+// Docs Link --> https://docs.google.com/document/d/19fo_5BxNyy3te0x6FGu_2t4MRXy4AlcR/edit?tab=t.0
+
+// Problem 1
+// 1. Implement Stack With Linked List - Coding Ninjas --> https://www.naukri.com/code360/problems/implement-stack-with-linked-list_630475
+
+// Problem 2
+// 1. Implement a Queue - Coding Ninjas --> https://www.naukri.com/code360/problems/queue-using-array-or-singly-linked-list_2099908
+
+// Problem 3
+// 1. Reverse First K elements of Queue - Coding Ninjas --> https://www.naukri.com/code360/problems/reverse-first-k-elements-of-queue_982771
+
+// Problem 4
+// 1. Min Stack - Coding Ninjas --> https://www.naukri.com/code360/problems/min-stack_3843991
+
+// Problem 5
+// 1. Kevin’s stack problem - Coding Ninjas --> https://www.naukri.com/code360/problems/kevin-s-stack-problem_1169465
