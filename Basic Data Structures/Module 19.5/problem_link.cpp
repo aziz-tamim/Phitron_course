5748d53096d96b5906b9fb12fee48a460bd87e88
abdul-aziz <tamimaziz2007@gmail.com>
2025-08-01 09:04:27 +0600

    update

---
+++ Basic Data Structures/Module 19.5/problem_link.cpp
@@ -0,0 +1,26 @@
+// Docs Link --> https://docs.google.com/document/d/1-c1LMMR8-mpqFAAwtfXRrgMkaObAlb-k/edit?tab=t.0
+
+
+// Root Equals Sum of Children [Easy] 
+// Problem link --> https://leetcode.com/problems/root-equals-sum-of-children/
+
+
+// Univalued Binary Tree [Easy] 
+// Problem link --> https://leetcode.com/problems/univalued-binary-tree/
+
+
+// Leaf-Similar Trees [Easy] 
+// Problem link --> https://leetcode.com/problems/leaf-similar-trees/
+
+// Same Tree  [Easy] 
+// Problem link --> https://leetcode.com/problems/same-tree/
+
+
+// Diameter of Binary Tree [Easy] 
+// Problem link --> https://leetcode.com/problems/diameter-of-binary-tree/description/
+
+
+// Binary Tree Right Side View  [Medium] 
+// Problem link --> https://leetcode.com/problems/binary-tree-right-side-view/description/
+
+
