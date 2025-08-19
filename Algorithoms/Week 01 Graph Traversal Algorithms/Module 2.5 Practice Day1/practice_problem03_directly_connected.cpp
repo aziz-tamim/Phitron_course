26fa1ef4604b817c6b9781f0017bb03878fa5224
abdul-aziz <tamimaziz2007@gmail.com>
2025-08-19 21:02:42 +0600

    update

--- Algorithoms/Week 01 Graph Traversal Algorithms/Module 2.5 Practice Day1/practice_problem03_directly_connected.cpp
+++ Algorithoms/Week 01 Graph Traversal Algorithms/Module 2.5 Practice Day1/practice_problem03_directly_connected.cpp
@@ -2,6 +2,19 @@
 using namespace std;
 int main()
 {
-    
+    int n,e;
+    cin >> n >> e;
+    vector<int> adj_list[n];
+
+    while(e--)
+    {
+        int a,b;
+        cin >> a >> b;
+        adj_list[a].push_back(b);
+        adj_list[b].push_back(a);
+    }
+    int l;
+    cin >> l;
+    cout << adj_list[l].size() << endl;
     return 0;
 }
