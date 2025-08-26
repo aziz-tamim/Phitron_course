31cf335eb4c0d73a8db73a0dde83091889b645d9
abdul-aziz <tamimaziz2007@gmail.com>
2025-08-26 09:09:46 +0600

    update

---
+++ Algorithoms/Week 02 Cycle Detection & dijkstra/Module 06 Cycle Detection/detect_cycle_in_directed_graph_using_dfs.cpp
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h>
+using namespace std;
+bool vis[105];
+vector<int> adj_list[105];
+bool pathvis[105];
+bool cycle;
+
+void dfs(int src)
+{
+    vis[src] = true;
+    pathvis[src] = true;
+    for(int child : adj_list[src])
+    {
+
+        if(vis[child] && pathvis[child])
+            cycle = true;
+        if(!vis[child])
+        {
+            pathvis[child] = src;
+            dfs(child);
+        }
+    }
+    // return
+    pathvis[src] = false;
+}
+
+int main()
+{
+    int n,e;
+    cin >> n >> e;
+    while(e--)
+    {
+        int a,b;
+        cin >> a >> b;
+        adj_list[a].push_back(b);
+    }
+    memset(vis,false,sizeof(vis));
+    memset(pathvis,-1,sizeof(pathvis));
+    cycle = false;
+    for(int i=0; i<n; i++)
+    {
+        if(!vis[i])
+            dfs(i);
+    }
+    // for(int i=0; i<n; i++)
+    // {
+    //     cout << i << " parent " << pathvis[i] << endl;
+    // }
+    if(cycle)
+        cout << "Cycle Detected\n";
+    else
+        cout << "No Cycle\n";
+    return 0;
+}
