25a1796480de03e470c687acce0da79f79f64dc3
abdul-aziz <tamimaziz2007@gmail.com>
2025-08-27 12:49:19 +0600

    update

---
+++ Algorithoms/Week 02 Cycle Detection & dijkstra/Module 05 Problem solving with BFS and DFS/problem_link.cpp
@@ -0,0 +1,27 @@
+// Problem Link --> https://docs.google.com/document/d/1QBllWHoRSyuKG7xZru_t0I4TSjWgPFoHfy2aZG3gbQI/edit?tab=t.0
+
+
+// LeetCode Problem List:
+
+// Island Perimeter   [Easy] 
+// Problem link --> https://leetcode.com/problems/island-perimeter/
+
+
+// Find if path exists in graph   [Easy]
+// Problem link --> https://leetcode.com/problems/find-if-path-exists-in-graph/
+
+
+// Max area of island   [Medium]
+// Problem link--> https://leetcode.com/problems/max-area-of-island/
+
+
+// Number of islands   [Medium]
+// Problem link --> https://leetcode.com/problems/number-of-islands/
+
+
+// Count sub islands   [Medium]
+// Problem link --> https://leetcode.com/problems/count-sub-islands/
+
+
+// Number of closed islands   [Medium]
+// Problem link --> https://leetcode.com/problems/number-of-closed-islands/
--- Algorithoms/Week 02 Cycle Detection & dijkstra/Module 6.5 Practice Day 01/question_link.cpp
+++ Algorithoms/Week 02 Cycle Detection & dijkstra/Module 6.5 Practice Day 01/question_link.cpp
@@ -1,1 +1,57 @@
 // question link --> https://docs.google.com/document/d/1tJhelqibvLQYJjak9tZ5Hxl1EhEuZD0D/edit?tab=t.0
+
+// Problem Links:
+
+// Detect cycle in an undirected graph [GFG] - [Solve this using BFS]
+// problem link --> https://www.geeksforgeeks.org/problems/detect-cycle-in-an-undirected-graph/1?utm_source=geeksforgeeks&utm_medium=article_practice_tab&utm_campaign=article_practice_tab
+
+// Detect cycle in an undirected graph [GFG] - [Solve this using DFS]
+// problem link --> https://www.geeksforgeeks.org/problems/detect-cycle-in-an-undirected-graph/1?utm_source=geeksforgeeks&utm_medium=article_practice_tab&utm_campaign=article_practice_tab
+
+
+// Course Schedule  [Medium]
+// Problem link --> https://leetcode.com/problems/course-schedule/
+
+
+// Find Eventual Safe States  [Medium]
+// Problem link --> https://leetcode.com/problems/find-eventual-safe-states/
+	
+// Counting rooms [CSES] 
+// Problem link --> https://cses.fi/problemset/task/1192
+
+// XPSC Problem Links:
+
+// A. Fashionable Array
+// problem link --> https://codeforces.com/problemset/problem/2110/A
+// A. Dr. TC
+// problem link --> https://codeforces.com/problemset/problem/2106/A
+
+
+// Extra Problems From Mod 5 & 6(Optional):
+
+// Maximum number of fish in a grid  [Medium]
+// Problem link --> https://leetcode.com/problems/maximum-number-of-fish-in-a-grid/description/
+
+
+// Surrounded Regions  [Medium]
+// Problem link --> https://leetcode.com/problems/surrounded-regions/description/
+
+
+// Battleships in a Board  [Medium]
+// Problem link --> https://leetcode.com/problems/battleships-in-a-board/description/
+
+
+// Minimum Number of Vertices to Reach All Nodes 
+// Problem link --> https://leetcode.com/problems/minimum-number-of-vertices-to-reach-all-nodes/
+
+
+// Shortest Path in Binary Matrix 
+// Problem link --> https://leetcode.com/problems/shortest-path-in-binary-matrix/
+
+
+// Number of Enclaves  [Medium] 
+// Problem link --> https://leetcode.com/problems/number-of-enclaves/
+
+
+// Number of Provinces  [Medium]
+// Problem link --> https://leetcode.com/problems/number-of-provinces/
