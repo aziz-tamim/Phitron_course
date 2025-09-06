e3b4cb7b34dfae147ab4c9908ae0f582a6277967
abdul-aziz <tamimaziz2007@gmail.com>
2025-09-06 13:22:36 +0600

    update

--- Basic Data Structures/Week 03 Doubly Linked List/Module 10/cycle_detect_in_linked_list.cpp
+++ Basic Data Structures/Week 03 Doubly Linked List/Module 10/cycle_detect_in_linked_list.cpp
@@ -30,7 +30,8 @@ int main()
     Node* slow = head;
     Node* fast = head;
     bool flag = false;
-    while(fast != NULL && fast->next != NULL){
+    while(fast != NULL && fast->next != NULL)
+    {
         slow = slow->next;
         fast = fast->next->next;
         if(slow == fast)
