fbc3850ecb6e520a702d625c11807173897ab5ad
abdul-aziz <tamimaziz2007@gmail.com>
2025-09-15 15:46:34 +0600

    update

--- Algorithoms/Week 04 More Problem Solveing with graph/Module 16 Final_Exam/Tetranacci_Number_l.cpp
+++ Algorithoms/Week 04 More Problem Solveing with graph/Module 16 Final_Exam/Tetranacci_Number_l.cpp
@@ -1,18 +1,18 @@
 // problem link --> https://www.hackerrank.com/contests/final-exam-a-introduction-to-algorithms-a-batch-07/challenges/tetranacci-number-a-easy-version/problem
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 int tetranacci(int n)
 {
-    if(n==0)
+    if (n == 0)
         return 0;
-    if(n==1)
+    if (n == 1)
         return 1;
-    if(n==2)
+    if (n == 2)
         return 1;
-    if(n == 3)
+    if (n == 3)
         return 2;
-    return tetranacci(n-1) + tetranacci(n-2) + tetranacci(n-3) + tetranacci(n-4);
-}    
+    return tetranacci(n - 1) + tetranacci(n - 2) + tetranacci(n - 3) + tetranacci(n - 4);
+}
 int main()
 {
     int n;
@@ -20,5 +20,3 @@ int main()
     cout << tetranacci(n) << endl;
     return 0;
 }
-
-
