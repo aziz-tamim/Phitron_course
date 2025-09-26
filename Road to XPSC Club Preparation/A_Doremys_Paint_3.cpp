41e82664e45178b20b861038f0e0464b34d848b2
abdul-aziz <tamimaziz2007@gmail.com>
2025-09-26 12:15:36 +0600

    update

---
+++ Road to XPSC Club Preparation/A_Doremys_Paint_3.cpp
@@ -0,0 +1,42 @@
+// problem link --> https://codeforces.com/contest/1890/problem/A
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        map<int,int> mp;
+        int n;
+        cin >> n;
+        int a[n];
+        for(int i=0; i<n; i++)
+        {
+            cin >> a[i];
+            mp[a[i]]++;
+        }
+        if(mp.size() > 2)
+            cout << "No\n";
+        else
+        {
+            if(mp.size() == 2)
+            {
+                vector<int> v;
+                for(auto p : mp)
+                {
+                    v.push_back(p.second);
+                }
+                if(abs(v[0] - v[1]) > 1)
+                    cout << "No\n";
+                else
+                    cout << "Yes\n";
+            }
+            else
+                cout << "Yes\n";
+        }
+    }
+    return 0;
+}
---
+++ Road to XPSC Club Preparation/A_Simple_Palindrome.cpp
@@ -0,0 +1,28 @@
+// problem link --> https://codeforces.com/contest/2005/problem/A
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        vector<char> v = {'a','e','i','o','u'};
+        string ans = "";
+        int i=0;
+        while(n--)
+        {
+            ans += v[i];
+            i++;
+            if(i==5) i = 0;
+        }
+        sort(ans.begin(),ans.end());
+        cout << ans << "\n";
+    }
+    
+    return 0;
+}
