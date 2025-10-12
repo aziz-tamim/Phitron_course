f48e0e4b863506f1b293912e9c71aa86eb9be00b
abdul-aziz <tamimaziz2007@gmail.com>
2025-10-12 08:29:03 +0600

    update

--- Problem_solving_club/CodeChef_Contest/First_contest/C_Make_Subarray.cpp
+++ Problem_solving_club/CodeChef_Contest/First_contest/C_Make_Subarray.cpp
@@ -1,42 +1,90 @@
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(nullptr);
+//     int tc;
+//     cin >> tc;
+//     while (tc--)
+//     {
+//         int num;
+//         string str;
+//         cin >> num >> str;
+
+//         int val1 = -1;
+//         int val2 = -1;
+//         for(int i=0; i<num; i++)
+//         {
+//             if(str[i] == '1')
+//             {
+//                 if(val1 == -1)
+//                     val1 = i;
+//                 val2 = i;
+
+//             }
+//         }
+//         if(val1 == -1)
+//             cout << 0 << "\n";
+//         else
+//         {
+//             int cnt = 0;
+//             for(int i=val1; i<= val2; i++)
+//             {
+//                 if(str[i] == '0')
+//                     cnt++;
+//             }
+//             cout << cnt << '\n';
+//         }
+//     }
+    
+//     return 0;
+// }
+
 #include<bits/stdc++.h>
 using namespace std;
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
-    int tc;
-    cin >> tc;
-    while (tc--)
+    int t;
+    cin >> t;
+    while(t--)
     {
-        int num;
-        string str;
-        cin >> num >> str;
-
-        int val1 = -1;
-        int val2 = -1;
-        for(int i=0; i<num; i++)
+        int n;
+        cin >> n;
+        string s;
+        cin >> s;
+        int first1 = -1, last1 = -1;
+        for(int i=0; i<n; i++)
         {
-            if(str[i] == '1')
+            if(s[i] == '1')
             {
-                if(val1 == -1)
-                    val1 = i;
-                val2 = i;
-
+                first1 = i;
+                break;
             }
         }
-        if(val1 == -1)
-            cout << 0 << "\n";
-        else
+        for(int i=n-1; i>=0; i--)
         {
-            int cnt = 0;
-            for(int i=val1; i<= val2; i++)
+            if(s[i] == '1')
             {
-                if(str[i] == '0')
-                    cnt++;
+                last1 = i;
+                break;
             }
-            cout << cnt << '\n';
         }
+        int ans = 0;
+        if(first1 == -1)
+        {
+            cout << 0 << endl;
+            continue;
+        }
+
+        for(int i=first1; i<=last1; i++)
+        {
+            if(s[i] == '0')
+                ans++;
+        }
+        cout << ans << endl;
     }
-    
     return 0;
 }
--- Problem_solving_club/CodeChef_Contest/First_contest/adjacent_sums.cpp
+++ Problem_solving_club/CodeChef_Contest/First_contest/adjacent_sums.cpp
@@ -1,41 +1,35 @@
-// #include <bits/stdc++.h>
-// using namespace std;
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int n;
+        cin >> n;
+        vector<long long> a(n);
+        for(int i=0; i<n; i++)
+        {
+            cin >> a[i];
+        }
+        vector<long long> dp(n); // i index porjonto good array banate minimum operation.
 
-// int main() {
-//     ios_base::sync_with_stdio(false);
-//     cin.tie(nullptr);
-
-//     int T;
-//     cin >> T;
-//     while(T--) {
-//         int N;
-//         cin >> N;
-//         vector<long long> A(N);
-//         for(int i = 0; i < N; i++) cin >> A[i];
-
-//         long long ops = 0;
-
-//         if(N == 2) {
-//             // Special case: only two elements
-//             ops = min(abs(A[0]-A[1]), min(A[0], A[1]));
-//         } else {
-//             // First element
-//             ops += min(abs(A[0]-A[1]), A[0]);
-
-//             // Middle elements
-//             for(int i = 1; i < N-1; i++) {
-//                 long long op_sum = abs(A[i] - (A[i-1]+A[i+1]));
-//                 long long op_zero = A[i];
-//                 ops += min(op_sum, op_zero);
-//             }
-
-//             // Last element
-//             ops += min(abs(A[N-1]-A[N-2]), A[N-1]);
-//         }
-
-//         cout << ops << "\n";
-//     }
-
-//     return 0;
-// }
-
+        dp[0] = a[0];
+        for(int i=1; i<n; i++)
+        {
+            long long cost1 = a[i] + dp[i-1];
+            if(i-1>=0)
+            {
+                long long cost2 = abs(a[i]-a[i-1]);
+                if(i-2>=0) cost2+=a[i-2];
+                if(i-3>=0) cost2+=dp[i-3];
+                dp[i] = min(cost1, cost2);
+            }
+        }
+        cout << dp[n-1] << "\n";
+    }
+    return 0;
+}
