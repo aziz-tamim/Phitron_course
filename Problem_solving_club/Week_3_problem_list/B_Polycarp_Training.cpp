000527343c4d16be879551678b114d700c390e4d
abdul-aziz <tamimaziz2007@gmail.com>
2025-10-30 16:16:44 +0600

    update

--- Problem_solving_club/Week_3_problem_list/B_Polycarp_Training.cpp
+++ Problem_solving_club/Week_3_problem_list/B_Polycarp_Training.cpp
@@ -21,26 +21,26 @@
 //     return 0;
 // }
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     int n;
-    cin >>n;
+    cin >> n;
     multiset<int> ml;
-    for(int i=0; i<n; i++)
+    for (int i = 0; i < n; i++)
     {
         int x;
         cin >> x;
         ml.insert(x);
     }
     int ans = 0, problem = 1;
-    while(!ml.empty())
+    while (!ml.empty())
     {
         auto LB = ml.lower_bound(problem);
-        if(LB != ml.end())
+        if (LB != ml.end())
         {
             ans++;
             ml.erase(LB);
--- Problem_solving_club/Week_3_problem_list/C_Train_and_Queries.cpp
+++ Problem_solving_club/Week_3_problem_list/C_Train_and_Queries.cpp
@@ -1,32 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
+int main()
+{
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     int tc;
     cin >> tc;
-    while(tc--)
+    while (tc--)
     {
         int a, b;
         cin >> a >> b;
         vector<int> arr(a);
-        map<int,int> fst, lst;
+        map<int, int> fst, lst;
 
-        for(int i = 0; i< a; i++)
+        for (int i = 0; i < a; i++)
         {
             cin >> arr[i];
-            if(fst.find(arr[i]) ==fst.end())
+            if (fst.find(arr[i]) == fst.end())
                 fst[arr[i]] = i;
             lst[arr[i]] = i;
         }
-        for(int i=0 ; i<b;i++)
+        for (int i = 0; i < b; i++)
         {
             int x, y;
             cin >> x >> y;
-            if(fst.find(x) == fst.end() ||fst.find(y) == fst.end())
+            if (fst.find(x) == fst.end() || fst.find(y) == fst.end())
                 cout << "NO" << "\n";
-            else if(fst[x] <= lst[y])
+            else if (fst[x] <= lst[y])
                 cout << "YES" << "\n";
             else
                 cout << "NO" << "\n";
