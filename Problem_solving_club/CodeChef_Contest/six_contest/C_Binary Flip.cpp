1e22c052e45a844ae6bc9eab9911cc68b521af67
abdul-aziz <tamimaziz2007@gmail.com>
2025-11-16 08:15:14 +0600

    update

--- Problem_solving_club/CodeChef_Contest/six_contest/C_Binary Flip.cpp
+++ Problem_solving_club/CodeChef_Contest/six_contest/C_Binary Flip.cpp
@@ -1,27 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
     int t;
     cin >> t;
-    while(t--)
+    while (t--)
     {
         int n;
         string s;
         cin >> n >> s;
         int cnt1 = 0, cnt2 = 0;
-        for(int i = 0; i < n-1; i++)
+        for (int i = 0; i < n - 1; i++)
         {
-            if(s[i]=='0' && s[i+1]=='0')
+            if (s[i] == '0' && s[i + 1] == '0')
                 cnt1++;
-            if(s[i]=='1' && s[i+1]=='1')
+            if (s[i] == '1' && s[i + 1] == '1')
                 cnt2++;
         }
 
-        if(cnt2 >= cnt1)
+        if (cnt2 >= cnt1)
         {
             cout << 0 << "\n";
         }
