72fb9f4eb78cf8e2bcba4ab32ece96225d3126b7
abdul-aziz <tamimaziz2007@gmail.com>
2025-11-20 15:05:19 +0600

    update'

---
+++ Problem_solving_club/Week_6_Math_Number_Theory/Problem_list/U_The_Attack_of_Queen.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int tc;
+    cin >> tc;
+    while(tc--)
+    {
+        ll n,x,y;
+        cin >> n >> x>> y;
+        ll lfs = y-1;
+        ll rgs = n-y;
+        ll ups = x-1;
+        ll dns = n-x;
+
+        ll upl = min(x-1, y-1);
+        ll upr = min(x-1, n-y);
+        ll dnl = min(n-x, y-1);
+        ll dnr = min(n-x, n-y);
+
+        ll ans = lfs + rgs + ups + dns + upl + upr+ dnl + dnr;
+        cout << ans << '\n';
+    }
+    return 0;
+}
