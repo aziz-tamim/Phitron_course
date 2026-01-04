a145bc72bf489bb6b4a653c8b2f2cc27b8db3bca
abdul-aziz <tamimaziz2007@gmail.com>
2026-01-04 11:12:15 +0600

    update

--- Problem_solving_club/CodeChef_Contest/Ten_Contest/Make An AP.cpp
+++ Problem_solving_club/CodeChef_Contest/Ten_Contest/Make An AP.cpp
@@ -7,29 +7,29 @@ int main()
     cin.tie(nullptr);
     int tc;
     cin >> tc;
-    while(tc--)
+    while (tc--)
     {
         ll n;
         cin >> n;
         vector<ll> arr(n);
-        for(int i=0; i<n; i++)
+        for (int i = 0; i < n; i++)
             cin >> arr[i];
 
-        if(n <= 2)
+        if (n <= 2)
         {
             cout << 0 << "\n";
             continue;
         }
         ll vinno = 0;
-        for(int i=1; i < n; i++)
+        for (int i = 1; i < n; i++)
         {
             ll diff = arr[i] - arr[i - 1];
-            if(i == 1)
+            if (i == 1)
                 vinno = diff;
             else
                 vinno = __gcd(vinno, diff);
         }
-        ll sum = (arr[n-1] - arr[0])/ vinno+1;
+        ll sum = (arr[n - 1] - arr[0]) / vinno + 1;
         ll ans = sum - n;
         cout << ans << "\n";
     }
