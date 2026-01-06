7cf66ae5a0061b3c4dee6536389603036ca30026
abdul-aziz <tamimaziz2007@gmail.com>
2026-01-06 09:32:34 +0600

    update

---
+++ Problem_solving_club/XPSC_Super_Zone/Speed Contest - 9/B - Breaking Sticks.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int n;
+        cin >> n;
+        int ans = 0;
+        for(int i=0; i<n; i++)
+        {
+            int x;
+            cin >> x;
+            ans = ans +(x-1);
+        }
+        cout << ans << "\n";
+    }
+    return 0;
+}
---
+++ Problem_solving_club/XPSC_Super_Zone/Speed Contest - 9/first.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n, m;
+    cin >> n >>m;
+    if(m>n)
+        cout << "Yes" << "\n";
+    else
+        cout << "No" << "\n";
+    return 0;
+}
