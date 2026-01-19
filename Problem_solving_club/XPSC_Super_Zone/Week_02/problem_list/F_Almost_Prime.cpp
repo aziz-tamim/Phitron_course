4ac0f31be19918a1fcc18b7bdfaee9510019916a
abdul-aziz <tamimaziz2007@gmail.com>
2026-01-19 15:36:52 +0600

    update

---
+++ Problem_solving_club/XPSC_Super_Zone/Week_02/problem_list/F_Almost_Prime.cpp
@@ -0,0 +1,55 @@
+#include<bits/stdc++.h>
+using namespace std;
+const int maxN = 3005;
+vector<int> allPrime;
+void sieve()
+{
+    vector<bool> prime(maxN+1, true);
+    prime[0] = prime[1] = false;
+    for(int i=2; i*i<= maxN; i++)
+    {
+        if(prime[i])
+        {
+            for(int j= i+i; j<=maxN; j+= i)
+                prime[j] = false;
+        }
+    }
+    for(int i=2; i<=maxN; i++)
+    {
+        if(prime[i])
+        {
+            allPrime.push_back(i);
+        }
+    }
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    sieve();
+    int n;
+    cin >> n;
+    int num = 2;
+    int primeCnt = 0;
+    while(num <=n)
+    {
+        int tmp = num;
+        map<int,int> cnt;
+        int idx = 0;
+        while(tmp > 1 && idx <allPrime.size())
+        {
+            while(tmp % allPrime[idx] == 0)
+            {
+                cnt[allPrime[idx]]++;
+                tmp /= allPrime[idx];
+            }
+            idx++;
+        }
+        if(cnt.size() == 2)
+            primeCnt++;
+        num++;
+    }
+    cout << primeCnt << "\n";
+    return 0;
+}
+
--- Problem_solving_club/XPSC_Super_Zone/Week_02/problem_list/G_Bachgold_Problem.cpp
+++ Problem_solving_club/XPSC_Super_Zone/Week_02/problem_list/G_Bachgold_Problem.cpp
@@ -27,11 +27,10 @@
 
 #include<bits/stdc++.h>
 using namespace std;
-
-int main() {
+int main()
+{
     int n;
     cin >> n;
-
     if(n % 2 == 0)
     {
         cout << n/2 << "\n";
