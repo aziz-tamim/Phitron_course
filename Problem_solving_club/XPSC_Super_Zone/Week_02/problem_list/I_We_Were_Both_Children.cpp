d7c4d48617d53d36839ee17e3cd4854fb016659e
abdul-aziz <tamimaziz2007@gmail.com>
2026-01-21 11:37:00 +0600

    update

--- Problem_solving_club/XPSC_Super_Zone/Week_02/problem_list/I_We_Were_Both_Children.cpp
+++ Problem_solving_club/XPSC_Super_Zone/Week_02/problem_list/I_We_Were_Both_Children.cpp
@@ -8,23 +8,26 @@ int main()
     cin >> tc;
     while(tc--)
     {
-        int n;      
+        int n;
         cin >> n;
         vector<int> frog(n);
         for(int i=0; i<n; i++)
             cin >> frog[i];
  
-        vector<int> frogcnt(n+1, 0);
-        for(int i=0; i<n; i++)
-        {
-            // if(frog[i] > n)
-            //     continue;
-            for(int j=frog[i]; j<=n; j+= frog[i])
-                frogcnt[j]++;
-        }
+        map<int,int> frogcnt;
+        for(int x :frog)
+            frogcnt[x]++;
+
+        vector<int> trpcnt(n+1, 0);
         int ans = 0;
-        for(int i=0; i<=n; i++)
-            ans = max(ans, frogcnt[i]);
+        for(int i=1; i<=n; i++)
+        {
+            for(int j=i; j<=n; j+=i)
+            {
+                trpcnt[j] += frogcnt[i];
+                ans = max(ans, trpcnt[j]);
+            }
+        }
         cout << ans << "\n";
     }
     return 0;
