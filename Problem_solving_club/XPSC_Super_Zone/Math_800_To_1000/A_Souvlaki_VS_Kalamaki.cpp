68c639999116746cc7f78639080540e3438b0cc9
abdul-aziz <tamimaziz2007@gmail.com>
2026-01-25 08:42:30 +0600

    Update

--- Problem_solving_club/XPSC_Super_Zone/Math_800_To_1000/A_Souvlaki_VS_Kalamaki.cpp
+++ Problem_solving_club/XPSC_Super_Zone/Math_800_To_1000/A_Souvlaki_VS_Kalamaki.cpp
@@ -11,23 +11,19 @@ int main()
     {
         int n;
         cin >> n;
-        vector<int> arr(n);
+        int arr[n+1];
         for(int i=0; i<n; i++)
             cin >> arr[i];
-        sort(arr.begin(),arr.end());
-        bool ok = true;
-        for(int i=1; i<n; i+=2)
+        sort(arr+1, arr+n+1);
+        for(int i=2; i<=n-1; i+=2)
         {
-            if(arr[i] != arr[i-1])
+            if(arr[i] != arr[i+1])
             {
-                ok = false;
-                break;
+                cout << "NO" << "\n";
+                return;
             }
         }
-        if(ok)
-            cout << "YES" << '\n';
-        else
-            cout << "NO" << '\n';
+        cout << "YES" << '\n';
     }
     return 0;
 }
