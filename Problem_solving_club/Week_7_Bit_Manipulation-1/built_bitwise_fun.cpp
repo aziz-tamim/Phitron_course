9e4298855df429ab991e8b8bc940f2de2a1508bd
abdul-aziz <tamimaziz2007@gmail.com>
2026-01-31 13:13:43 +0600

    update

--- Problem_solving_club/Week_7_Bit_Manipulation-1/built_bitwise_fun.cpp
+++ Problem_solving_club/Week_7_Bit_Manipulation-1/built_bitwise_fun.cpp
@@ -6,9 +6,9 @@ bool check_kth_bit_on_or_off(int n, int k)
 }
 void print_on_and_off_bits(int n)
 {
-    for (int k = 31; k >= 0; k--)
+    for(int k = 31; k >= 0; k--)
     {
-        if (check_kth_bit_on_or_off(n, k))
+        if(check_kth_bit_on_or_off(n, k))
             cout << 1 << " ";
         else
             cout << 0 << " ";
