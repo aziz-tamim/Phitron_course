78897fdbcfbea3d426d514b406fa6f1ff24e15e0
abdul-aziz <tamimaziz2007@gmail.com>
2026-02-05 12:24:05 +0600

    update

--- Problem_solving_club/XPSC_Super_Zone/week_03_Bit Manipulation-2/R_Gorilla_and_the_Exam.cpp
+++ Problem_solving_club/XPSC_Super_Zone/week_03_Bit Manipulation-2/R_Gorilla_and_the_Exam.cpp
@@ -12,7 +12,33 @@ int main()
     cin >> tc;
     while(tc--)
     {
-        
+        int n,m;
+        cin >> n >> m;
+        vector<int> arr(n);
+        map<int,int> mp;
+        for(int i=0; i<n; i++)
+        {
+            cin >> arr[i];
+            mp[arr[i]]++;
+        }
+        vector<int> freq;
+        for(auto x : mp)
+            freq.push_back(x.second);
+            
+        sort(freq.begin(), freq.end());
+        int dis = freq.size();
+        for(auto x : freq)
+        {
+            if(m >= x)
+            {
+                m -= x;
+                dis--;
+            }
+            else
+                break;
+        }
+        int ans = max(1, dis);
+        cout << ans << nl;
     }
     return 0;
 }
