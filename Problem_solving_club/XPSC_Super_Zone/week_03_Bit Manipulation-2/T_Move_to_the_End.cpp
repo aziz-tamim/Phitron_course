869b32d20375a7f8bbf0120a6c3e9d9f2d7c4de3
abdul-aziz <tamimaziz2007@gmail.com>
2026-02-05 11:18:55 +0600

    update

--- Problem_solving_club/XPSC_Super_Zone/week_03_Bit Manipulation-2/T_Move_to_the_End.cpp
+++ Problem_solving_club/XPSC_Super_Zone/week_03_Bit Manipulation-2/T_Move_to_the_End.cpp
@@ -38,6 +38,8 @@
 #include<bits/stdc++.h>
 using namespace std;
 #define ll long long
+#define nl "\n"
+#define sp " "
 int main()
 {
     ios_base::sync_with_stdio(false);
@@ -46,34 +48,33 @@ int main()
     cin >> tc;
     while(tc--)
     {
-        int n; 
+        int n;
         cin >> n;
         vector<ll> arr(n);
-        vector<ll> suff(n+1, 0);
-        vector<ll> lft(n,-1);
+        vector<ll> suff(n+1,0);
+        vector<ll> pre(n);
         for(int i=0; i<n; i++)
         {
             cin >> arr[i];
             if(i == 0)
-                lft[i] = arr[i];
+                pre[i] = arr[i];
             else
-                lft[i] = max(lft[i-1], arr[i]);
+                pre[i] = max(pre[i-1], arr[i]);
         }
         for(int i = n-1; i>=0; i--)
         {
             suff[i] = suff[i+1] + arr[i];
         }
-        for(int i = 0; i < n; i++)
+        for(int j =0; j<n; j++)
         {
-            ll idx = n-1-i;
-            ll sum = suff[idx];
-            ll curr = arr[idx];
- 
-            if(idx > 0 && curr < lft[idx-1])
-                sum = sum - curr+lft[idx-1];
-            cout << sum << " ";
+            ll idx = n-1-j;
+            ll ans = suff[idx];
+            
+            if(idx > 0 && arr[idx] < pre[idx-1])
+                ans += pre[idx-1] - arr[idx];
+            cout << ans << sp;
         }
-        cout << "\n";
+        cout << nl;
     }
     return 0;
 }
