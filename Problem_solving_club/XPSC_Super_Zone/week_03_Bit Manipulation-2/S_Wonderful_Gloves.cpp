6cc2bada6264ef61c66711bde23bb4acafbb54cc
abdul-aziz <tamimaziz2007@gmail.com>
2026-02-06 11:53:06 +0600

    update

--- Problem_solving_club/XPSC_Super_Zone/week_03_Bit Manipulation-2/S_Wonderful_Gloves.cpp
+++ Problem_solving_club/XPSC_Super_Zone/week_03_Bit Manipulation-2/S_Wonderful_Gloves.cpp
@@ -12,9 +12,28 @@ int main()
     cin >> tc;
     while(tc--)
     {
-        int n,m;
+        int n, m;
         cin >> n >> m;
-        
+        vector<ll> l(n), r(n);
+        for(int i = 0; i<n; i++)
+            cin >> l[i];
+        for(int i=0; i<n; i++) 
+            cin >> r[i];
+            
+        vector<ll> lft(n), rigt(n);
+        for(int i=0; i< n; i++)
+        {
+            rigt[i] = min(l[i], r[i]);
+            lft[i] = max(l[i], r[i]);
+        }
+        sort(rigt.begin(), rigt.end(), greater<ll>());
+        ll ans = 0;
+        for(int i=0; i<n; i++)
+            ans += lft[i];
+        for(int i=0; i< m-1; i++)
+            ans += rigt[i];
+        ans += 1;
+        cout << ans << "\n";
     }
     return 0;
 }
