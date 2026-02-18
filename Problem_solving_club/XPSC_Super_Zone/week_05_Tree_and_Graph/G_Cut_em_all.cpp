a28391a4759d38580bd5648a3d96ec9667fbc730
abdul-aziz <tamimaziz2007@gmail.com>
2026-02-18 11:59:34 +0600

    update

---
+++ Problem_solving_club/XPSC_Super_Zone/week_05_Tree_and_Graph/G_Cut_em_all.cpp
@@ -0,0 +1,46 @@
+// AUTHOR :- ABDUL AZIZ TAMIM (tamimaziz2007@gmail.com)
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define nl "\n"
+#define sp " "
+const int maxN = 1e5+5;
+vector<int> a[maxN];
+int n,ans = 0;a
+int dfs(int x, int par)
+{
+    int total = 1;
+    int minTotal = 0;
+    for(int z : a[x])
+    {
+        if(z == par)
+            continue;
+        minTotal = dfs(z,x);
+        if(minTotal%2 == 0)
+            ans++;
+        else
+            total += minTotal;
+    }
+    return total;
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cin >> n;
+    if(n%2 != 0)
+    {
+        cout << -1 << nl;
+        return 0;
+    }
+    for(int i=1; i<n; i++)
+    {
+        int x, y;
+        cin >> x >> y;
+        a[x].push_back(y);
+        a[y].push_back(x);
+    }
+    dfs(1,0);
+    cout << ans << nl;
+    return 0;
+}
---
+++ Problem_solving_club/XPSC_Super_Zone/week_05_Tree_and_Graph/H_Mahmoud_and_Ehab_and_the_bipartiteness.cpp
@@ -0,0 +1,44 @@
+// AUTHOR :- ABDUL AZIZ TAMIM (tamimaziz2007@gmail.com)
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define nl "\n"
+#define sp " "
+const int maxN = 1e5+5;
+vector<int> a[maxN];
+int n;
+ll fstCnt = 0, scndCnt = 0;
+ll visi[maxN];
+void dfs(int u, int par, int col)
+{
+    visi[u] = 1;
+    if(col == 1)
+        fstCnt++;
+    else
+        scndCnt++;
+    for(int x : a[u])
+    {
+        if(x == par)
+            continue;
+        dfs(x,u,3-col);
+    }
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cin >> n;
+    for(int i=1; i<n; i++)
+    {
+        int u,v;
+        cin >> u >> v;
+        a[u].push_back(v);
+        a[v].push_back(u);
+    }
+    dfs(1,0,1);
+    ll edge = fstCnt*scndCnt;
+    ll currEdge = n-1;
+    ll ans = edge-currEdge;
+    cout << ans << nl;
+    return 0;
+}
