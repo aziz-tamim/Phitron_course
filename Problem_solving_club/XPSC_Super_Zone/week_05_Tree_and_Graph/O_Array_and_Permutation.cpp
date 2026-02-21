b74b2a2c9d9d57aa14710d5ffca61a01ce878496
abdul-aziz <tamimaziz2007@gmail.com>
2026-02-21 07:29:18 +0600

    update

--- Problem_solving_club/XPSC_Super_Zone/week_05_Tree_and_Graph/O_Array_and_Permutation.cpp
+++ Problem_solving_club/XPSC_Super_Zone/week_05_Tree_and_Graph/O_Array_and_Permutation.cpp
@@ -1,84 +1,36 @@
-// // AUTHOR :- ABDUL AZIZ TAMIM (tamimaziz2007@gmail.com)
-// #include<bits/stdc++.h>
-// using namespace std;
-// #define ll long long
-// #define nl "\n"
-// #define sp " "
-// int main()
-// {
-//     ios_base::sync_with_stdio(false);
-//     cin.tie(nullptr);
-//     int tc;
-//     cin >> tc;
-//     while(tc--)
-//     {
-//         int n;
-//         cin >> n;
-//         vector<int> p(n), a(n);
-//         for(int i=0; i<n; i++)
-//             cin >> p[i];
-//         for(int i=0; i<n; i++)
-//             cin >> a[i];
-//         bool ok = true;
-//         if(a[0] != p[0])
-//             ok = false;
-//         for(int i=1; i<n && ok; i++)
-//         {
-//             if(a[i] != p[i] && a[i] != a[i-1])
-//                 ok = false;
-//         }
-//         if(ok)
-//             cout << "YES" << nl;
-//         else
-//             cout << "NO" << nl;
-//     }
-//     return 0;
-// }
-
-// AUTHOR :- ABDUL AZIZ TAMIM
+// AUTHOR :- ABDUL AZIZ TAMIM (tamimaziz2007@gmail.com)
 #include<bits/stdc++.h>
 using namespace std;
+#define ll long long
 #define nl "\n"
-
+#define sp " "
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
-
     int tc;
     cin >> tc;
     while(tc--)
     {
         int n;
         cin >> n;
-
         vector<int> p(n), a(n);
-        for(int i=0; i<n; i++) cin >> p[i];
-        for(int i=0; i<n; i++) cin >> a[i];
-
-        bool ok = true;
-
-        for(int i = 0; i < n; i++)
+        for(int i=0; i<n; i++)
+            cin >> p[i];
+        for(int i=0; i<n; i++)
+            cin >> a[i];
+        int i=0,j=0;
+        while(i<n && j<n)
         {
-            if(a[i] == p[i]) continue;
-
-            bool possible = false;
-
-            if(i > 0 && a[i] == a[i-1])
-                possible = true;
-
-            if(i < n-1 && a[i] == a[i+1])
-                possible = true;
-
-            if(!possible)
-            {
-                ok = false;
-                break;
-            }
+            if(p[i] == a[j])
+                j++;
+            else
+                i++;
         }
-
-        cout << (ok ? "YES" : "NO") << nl;
+        if(j==n)
+            cout << "YES" << nl;
+        else
+            cout << "NO" << nl;
     }
-
     return 0;
 }
