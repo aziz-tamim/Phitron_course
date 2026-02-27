566f45a2e1ba983b773444e6d0f2f5775cce1f44
abdul-aziz <tamimaziz2007@gmail.com>
2026-02-27 08:09:14 +0600

    update

--- Problem_solving_club/CodeChef_Contest/CodeChef_Starters_226/max_minus_min.cpp
+++ Problem_solving_club/CodeChef_Contest/CodeChef_Starters_226/max_minus_min.cpp
@@ -21,12 +21,15 @@ int main()
         a.erase(unique(a.begin(), a.end()), a.end());
         n = a.size();
         
-        for(int i=0; i<n-1; i++){
-            while(2*a[i] <= a[n-1]) a[i] *= 2;
+        for(int i=0; i<n-1; i++)
+        {
+            while(2*a[i] <= a[n-1])
+                a[i] *= 2;
         }
         sort(a.begin(), a.end());
         int ans = a[n-1] - a[0];
-        for(int i=0; i<n-1; i++){
+        for(int i=0; i<n-1; i++)
+        {
             int mx = a[i]*2;
             int mn = a[i+1];
             ans = min(ans, mx-mn);
--- Problem_solving_club/XPSC_Super_Zone/week_06_DP/I_Mortal_Kombat_Tower.cpp
+++ Problem_solving_club/XPSC_Super_Zone/week_06_DP/I_Mortal_Kombat_Tower.cpp
@@ -1,37 +1,107 @@
+// // AUTHOR :- ABDUL AZIZ TAMIM
+// #include<bits/stdc++.h>
+// using namespace std;
+// #define ll long long
+// #define nl "\n"
+// #define sp " "
+// int dp[200005][2];
+// int n;
+// vector<int> a;
+// int f(int i, int aij)
+// {
+//     if(i>n)
+//         return 0;
+//     if(dp[i][aij] != -1)
+//         return dp[i][aij];
+//     int ans = 1e9;
+//     if(aij == 0)
+//     {
+//         int fst_skip = 0;
+//         if(a[i] == 1)
+//             fst_skip = 1;
+//         fst_skip = fst_skip + f(i+1, 1);
+//         ans = min(ans, fst_skip);
+//         if(i+1 <= n)
+//         {
+//             int scnd_skip = 0;
+//             if(a[i] == 1)
+//                 scnd_skip = scnd_skip + 1;
+//             if(a[i+1] == 1)
+//                 scnd_skip = scnd_skip + i;
+//             scnd_skip = scnd_skip + 1;
+//             ans = min(ans,scnd_skip);
+//         }
+//     }
+//     else
+//     {
+//         ans = min(ans, f(i+1, 0));
+//         if(i+1 <= n)
+//             ans = min(ans, f(i+2, 0));
+//     }
+//     return dp[i][aij] = ans;
+// }
+// int main()
+// {
+//     ios_base::sync_with_stdio(false);
+//     cin.tie(nullptr);
+//     int tc;
+//     cin >> tc;
+//     while(tc--)
+//     {
+//         cin >> n;
+//         a.resize(n+2);
+//         for (int i = 1; i <= n; i++)
+//             cin >> a[i];
+//         memset(dp, -1, sizeof(dp));
+//         cout << f(1, 0) << '\n';
+//     }
+//     return 0;
+// }
+
+
 // AUTHOR :- ABDUL AZIZ TAMIM
 #include<bits/stdc++.h>
 using namespace std;
 #define ll long long
 #define nl "\n"
-#define sp " "
 int dp[200005][2];
 int n;
 vector<int> a;
 int f(int i, int aij)
 {
-    if(i>n)
+    if(i > n)
         return 0;
     if(dp[i][aij] != -1)
         return dp[i][aij];
     int ans = 1e9;
     if(aij == 0)
     {
-        int skip1 = (a[i] == 1 ? 1 : 0) + f(i+1, 1);
-        ans = min(ans, skip1);
+        int fst_skip = 0;
+        if(a[i] == 1)
+            fst_skip = 1;
+
+        fst_skip = fst_skip + f(i+1, 1);
+        ans = min(ans, fst_skip);
         if(i + 1 <= n)
         {
-            int skip2 = (a[i]==1 ? 1 : 0) + (a[i+1] == 1 ? 1 : 0) + f(i+2, 1);
-            ans = min(ans, skip2);
+            int scnd_skip = 0;
+            if(a[i] == 1)
+                scnd_skip = scnd_skip + 1;
+            if(a[i+1] == 1)
+                scnd_skip = scnd_skip + 1;
+            scnd_skip = scnd_skip + f(i+2,1);
+            ans = min(ans, scnd_skip);
         }
     }
     else
     {
         ans = min(ans, f(i+1, 0));
-        if (i+1 <= n)
+        if(i+1 <= n)
             ans = min(ans, f(i+2, 0));
     }
     return dp[i][aij] = ans;
 }
+
 int main()
 {
     ios_base::sync_with_stdio(false);
@@ -41,11 +111,15 @@ int main()
     while(tc--)
     {
         cin >> n;
-        a.resize(n+2);
-        for (int i = 1; i <= n; i++)
+        a.resize(n + 2);
+        for(int i=1; i<= n; i++)
             cin >> a[i];
-        memset(dp, -1, sizeof(dp));
-        cout << f(1, 0) << '\n';
+        for(int i=0; i <= n; i++)
+        {
+            dp[i][0] = -1;
+            dp[i][1] = -1;
+        }
+        cout << f(1, 0) << nl;
     }
     return 0;
 }
