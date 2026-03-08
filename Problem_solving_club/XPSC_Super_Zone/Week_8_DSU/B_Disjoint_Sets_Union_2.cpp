56f9518fec5132f4f1bca150d08f6a8c9076b1b7
abdul-aziz <tamimaziz2007@gmail.com>
2026-03-08 11:55:25 +0600

    update

--- Problem_solving_club/XPSC_Super_Zone/Week_8_DSU/B_Disjoint_Sets_Union_2.cpp
+++ Problem_solving_club/XPSC_Super_Zone/Week_8_DSU/B_Disjoint_Sets_Union_2.cpp
@@ -4,8 +4,49 @@ using namespace std;
 #define ll long long
 #define nl "\n"
 #define sp " "
-struct DSU{
-    
+struct DSU
+{
+    vector<int> par, rnk, siz, mini, maxi;
+    int c;
+    DSU(int n) : par(n+1,-1), rnk(n+1,0), siz(n+1,1),c(n), mini(n+1-1), maxi(n+1,-1)
+    {
+        for(int i=1; i<=n ;i++)
+        {
+            par[i] = i;
+            mini[i] = maxi[i] = i;
+        }
+    }
+    int find(int i)
+    {
+        return (par[i] == i ? i : (par[i] = find(par[i])));
+    }
+    bool same(int u, int v)
+    {
+        return find(u) == find(v);
+    }
+    int getSize(int u)
+    {
+        return siz[find(u)];
+    }
+    int cnt()
+    {
+        return c;
+    }
+    void merge(int u, int v)
+    {
+        if((u = find(u)) == (v=find(v)))
+            return;
+        else 
+            c--;
+        if(rnk[u] > rnk[v])
+            swap(u,v);
+        else if(rnk[u] == rnk[v])
+            rnk[v]++;
+        par[u] = par[v];
+        siz[v] += siz[u];
+        mini[v] = min(mini[u], mini[v]);
+        maxi[v] = max(maxi[u], maxi[v]);
+    }
 };
 int main()
 {
@@ -16,7 +57,7 @@ int main()
     DSU D(n);
     for(int i=1; i<=m; i++)
     {
-        int query;
+        string query;
         cin >> query;
         if(query == "union")
         {
@@ -28,6 +69,8 @@ int main()
         {
             int v;
             cin >> v;
+            v = D.find(v);
+            cout << D.mini[v] << " " << D.maxi[v] << " " << D.getSize(v) << '\n';
         }
     }
     return 0;
