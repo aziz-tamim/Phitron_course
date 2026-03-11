b96082a1acc2a323e25bcfc3209679aee4feeffb
abdul-aziz <tamimaziz2007@gmail.com>
2026-03-11 09:02:36 +0600

    update

---
+++ Problem_solving_club/XPSC_Super_Zone/Week_8_DSU/I_Cyclic_Components.cpp
@@ -0,0 +1,52 @@
+// AUTHOR :- ABDUL AZIZ TAMIM
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define nl "\n"
+#define sp " "
+const int maxN = 1e5+5;
+bool vis[maxN];
+vector<int> adj_list[maxN];
+int par[maxN];
+
+int cnt1,cnt2;
+void dfs(int src)
+{
+    vis[src] = true;
+    cnt1++;
+    cnt2 += adj_list[src].size();
+    for(int x : adj_list[src])
+    {
+        if(vis[x] == false)
+            dfs(x);
+    }
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n,e;
+    cin >> n >> e;
+    for(int i=0; i<e; i++)
+    {
+        int a,b;
+        cin >> a >> b;
+        adj_list[a].push_back(b);
+        adj_list[b].push_back(a);
+    }
+    memset(vis, false, sizeof(vis));
+    int cycle_com = 0;
+    for(int i=1; i<=n; i++)
+    {
+        if(vis[i] == false)
+        {
+            cnt1 = 0, cnt2 = 0;
+            dfs(i);
+            cnt2 /= 2;
+            if(cnt1 >= 3 && cnt1 == cnt2)
+                cycle_com++;
+        }
+    }
+    cout << cycle_com << nl;
+    return 0;
+}
