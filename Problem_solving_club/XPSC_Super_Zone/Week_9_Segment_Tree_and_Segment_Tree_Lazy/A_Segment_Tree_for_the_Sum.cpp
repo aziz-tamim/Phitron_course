f65e14d9cf4583aa43a201a9631eeb0318f716f9
abdul-aziz <tamimaziz2007@gmail.com>
2026-03-16 13:53:54 +0600

    update

---
+++ Problem_solving_club/XPSC_Super_Zone/Week_9_Segment_Tree_and_Segment_Tree_Lazy/A_Segment_Tree_for_the_Sum.cpp
@@ -0,0 +1,24 @@
+// AUTHOR :- ABDUL AZIZ TAMIM
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define nl "\n"
+#define sp " "
+const int maxN = 1e5+9;
+int a[maxN];
+void build(int n, int b, int e)
+{
+    
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n,m;
+    cin >> n >> m;
+    for(int i=0; i<=n;i++)
+    {
+        cin >> a[i];
+    }
+    return 0;
+}
