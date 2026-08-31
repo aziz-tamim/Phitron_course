9e4d06e3efe643269eabdfc4cb51bb4a21a49953
abdul-aziz <tamimaziz2007@gmail.com>
2026-08-31 14:40:21 +0600

    solve problem

---
+++ Problem_solving_club/CodeForces_contest/CF_Round_1090_Div4/A_The_67th_Integer_Problem.cpp
@@ -0,0 +1,20 @@
+// AUTHOR :- ABDUL AZIZ TAMIM
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define nl "\n"
+#define sp " "
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int tc;
+    cin >> tc;
+    while(tc--)
+    {
+        int x;
+        cin >> x;
+        cout << min(x+1, 67) << nl;
+    }
+    return 0;
+}
