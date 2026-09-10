08362a06bf8043c881a2b8bde4e1b64b209bf723
abdul-aziz <tamimaziz2007@gmail.com>
2026-09-10 13:56:17 +0600

    update

--- Problem_solving_club/CodeForces_contest/CF_Round_1090_Div4/E_The_67th_XOR_Problem.cpp
+++ Problem_solving_club/CodeForces_contest/CF_Round_1090_Div4/E_The_67th_XOR_Problem.cpp
@@ -1,5 +1,4 @@
 // AUTHOR :- ABDUL AZIZ TAMIM
-
 #include <bits/stdc++.h>
 using namespace std;
 #define ll long long
@@ -19,17 +18,16 @@ int main() {
 
     int tc;
     cin >> tc;
-    while (tc--) {
+    while(tc--)
+    {
         int n;
         cin >> n;
 
         vector<ll> a(n);
-
         for (auto &x : a)
             cin >> x;
 
         vector<Node> trie(1);
-
         auto insert = [&](ll x) {
             int node = 0;
 
@@ -48,35 +46,34 @@ int main() {
             int node = 0;
             ll res = 0;
 
-            for (int bit = 30; bit >= 0; bit--) {
+            for(int bit = 30; bit >= 0; bit--)
+            {
                 int b = (x >> bit) & 1;
 
                 // Prefer opposite bit
                 int want = b ^ 1;
 
-                if (trie[node].child[want] != -1) {
+                if (trie[node].child[want] != -1)
+                {
                     res |= (1LL << bit);
                     node = trie[node].child[want];
-                } else {
-                    node = trie[node].child[b];
                 }
+                else
+                    node = trie[node].child[b];
             }
-
             return res;
         };
 
         // Insert first element
         insert(a[0]);
-
         ll ans = 0;
 
-        for (int i = 1; i < n; i++) {
+        for (int i = 1; i < n; i++)
+        {
             ans = max(ans, getMaxXor(a[i]));
             insert(a[i]);
         }
-
         cout << ans << nl;
     }
-
     return 0;
 }
